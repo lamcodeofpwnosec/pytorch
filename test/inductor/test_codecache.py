427656d4b2d6c9c42f709903dd191b2334346981
Oguz Ulgen <oulgen@meta.com>
2024-10-26 11:25:17 -0700

    [RFC] Add caching heuristic for skipping fast graphs (#138938)
    Summary:
    
    Looking at scuba dataset, we can see that many graphs take less than 1s to compile. Caching is not worth it for those graphs. Lets add a heuristic.
    
    https://fburl.com/scuba/dynamo_compile/hbf39dli
    
    Test Plan: adhoc testing
    
    Differential Revision: D64995390

--- test/inductor/test_codecache.py
+++ test/inductor/test_codecache.py
@@ -444,6 +444,30 @@ class TestFxGraphCache(TestCase):
 
     @config.patch({"fx_graph_cache": True})
     @config.patch({"fx_graph_remote_cache": False})
+    @config.patch(
+        {"fx_graph_cache_minimum_inductor_compile_time_for_caching_ms": 100000}
+    )
+    def test_cache_refuse_save(self):
+        """
+        Test cache save refusal
+        """
+
+        def fn(x, y):
+            return x + y
+
+        with mock.patch("torch._utils_internal.is_fb_unit_test", return_value=False):
+            compiled_fn = torch.compile(fn, fullgraph=True)
+
+            x = torch.randn(4)
+            y = torch.randn(4)
+            compiled_fn(x, y)
+
+            self.assertEqual(counters["inductor"]["fxgraph_cache_miss"], 1)
+            self.assertEqual(counters["inductor"]["fxgraph_cache_hit"], 0)
+            self.assertEqual(counters["inductor"]["fxgraph_cache_refuse_save"], 1)
+
+    @config.patch({"fx_graph_cache": True})
+    @config.patch({"fx_graph_remote_cache": False})
     def test_generated_kernel_count(self):
         """
         Test that we bump the generated_kernel_count metric on a cache hit.
